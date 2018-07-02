c4f2fcc4a63f3d2e25a2d6ee0466bd105b06487c
Alessandro Marzialetti <setti93@gmail.com>
2018-07-02 18:42:25 +0200

    added some offsets

--- src/file_editor.hpp
+++ src/file_editor.hpp
@@ -22,11 +22,19 @@ struct Weapon {
   std::uint32_t current_ammo;
 };
 
-struct PlayerInfo {
+struct Player {
   enum offset : int {
+    e_current_map = 0x8,
+    e_current_song = 0xc,
+    e_horiz_position = 0x11,
+    e_vert_position = 0x15,
+    e_facing_direction = 0x18,
     e_max_health = 0x1c,
     e_whimsical_stars = 0x1e,
     e_current_health = 0x20,
+    e_current_weapon = 0x24,
+    e_equipped_items = 0x2c,
+    e_time = 0x34,
     e_weapons = 0x38,
     e_inventory = 0xd8,
     e_flags = 0x21c,
@@ -37,22 +45,22 @@ struct PlayerInfo {
   std::uint32_t maximum_health;
   std::uint32_t whimsical_stars;
   std::array<Weapon, 6> weapons;
-  std::array<std::uint32_t, 24> inventory;
+  std::array<std::uint32_t, 30> inventory;
 };
 
 class FileEditor {
-  std::array<FlagByte,
-             PlayerInfo::offset::e_flags_end - PlayerInfo::offset::e_flags>
+  std::array<FlagByte, Player::offset::e_flags_end - Player::offset::e_flags>
       m_flags;
   std::fstream m_file;
   int m_save_slot;
-  PlayerInfo m_player;
+  Player m_player;
   bool m_modified;
 
   void load_profile();
 
  public:
   FileEditor(const std::string& filename, int profile = 1);
+  ~FileEditor();
 
   void set_flag(int id, bool active);
   bool get_flag(int id);
